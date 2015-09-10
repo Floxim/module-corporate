0d5b02bd74b8a84c1c60bc78018a0f105815a3eb
Ilya <dubr.cola@gmail.com>
2015-09-10 15:50:18 +0300

    no more tabs!

--- Vacancy/Entity.php
+++ Vacancy/Entity.php
@@ -5,16 +5,5 @@ use Floxim\Floxim\System\Fx as fx;
 
 class Entity extends \Floxim\Main\Page\Entity
 {
-    public function getFormFields()
-    {
-        $fields = fx::collection(parent::getFormFields());
-        $fields->apply(function ($f) {
-            if (in_array($f['name'], array('salary_from', 'salary_to', 'currency', 'image'))) {
-                $f['tab'] = 3;
-            } elseif ($f['name'] === 'description') {
-                $f['tab'] = 1;
-            }
-        });
-        return $fields;
-    }
+    
 }
