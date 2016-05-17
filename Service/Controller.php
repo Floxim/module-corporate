7d7f75cde42931f0c4ce06045ce03e7e5ac5a4e8
Ilya <dubr.cola@gmail.com>
2016-05-17 21:23:53 +0300

    Service goes to floxim.corporate module

---
+++ Service/Controller.php
@@ -0,0 +1,9 @@
+<?php
+namespace Floxim\Corporate\Service;
+
+use Floxim\Floxim\System\Fx as fx;
+
+class Controller extends \Floxim\Main\Page\Controller 
+{
+
+}
---
+++ Service/Entity.php
@@ -0,0 +1,9 @@
+<?php
+namespace Floxim\Corporate\Service;
+
+use Floxim\Floxim\System\Fx as fx;
+
+class Entity extends \Floxim\Main\Page\Entity
+{
+
+}
